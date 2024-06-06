84938f15925a20abfa35d9d12fc39bb41826fe3a
mbystryk <mariya.bystryk@gmail.com>
2024-06-06 21:07:32 +0300

    Update task3.js

--- task3.js
+++ task3.js
@@ -2,9 +2,17 @@
 
 // spread:
 // is (...)
-//
-//
-//
+// is used to unpack elements of an iterable into places where multiple elements/arguments are expected.
+// example
+// const arr1 = [1, 2, 3];
+// const arr2 = [...arr1, 4, 5];
+// console.log(arr2); output: [1, 2, 3, 4, 5]
 //
 //
 // rest:
+// is used to collect multiple elements into a single entity, such as an array or object.
+// example
+// const [first, ...rest] = [1, 2, 3, 4];
+// console.log(first); // Output: 1
+// console.log(rest);  // Output: [2, 3, 4]
+// 
